687eeb5b11e93cc33a564a8626eb6830a2554d87
Works Chan <310019@163.com>
2015-11-24 23:42:57 +0800

    Update e41.c

--- e41.c
+++ e41.c
@@ -1,3 +1,8 @@
+/**
+ * Exercise 4-1. Write the function strindex(s,t) which returns the position of the rightmost occurrence of t in s, or -1 if
+ * there is none.
+ */
+
 #include <stdio.h>
 #include <string.h>
 
