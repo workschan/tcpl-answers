5b45535ab187d138f2876de1b37ced638879b0ce
Works Chan <310019@163.com>
2015-11-24 23:41:25 +0800

    Create e41.c

---
+++ e41.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+
+int strrindex(char s[], char t[]);
+
+int main()
+{
+    int i;
+    char s[] = "This is a test string.";
+    char t[] = "is";
+    i = strrindex(s, t);
+    printf("%d\n", i);
+    return 0;
+}
+
+int strrindex(char s[], char t[])
+{
+    int i, j, k, pos;
+    int ls = strlen(s);
+    int lt = strlen(t);
+
+    for (i = ls-1; i>=0; i--)
+    {
+        if (s[i] == t[lt-1]) {
+            pos = i-lt+1;
+            for (j=pos, k=0; j>-1 && t[k]!='\0' && s[j]==t[k]; j++, k++)
+                ;
+            if (k > 0 && t[k] == '\0')
+                return pos;
+        }
+    }
+    return -1;
+}
